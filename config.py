b5942f6e0505b5992d8477dad0a2868b37cfeee8
palyoniy83-lgtm <palyoniy83@gmail.com>
2026-07-11 21:24:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,24 @@
+# TMDB API ключ
+TMDB_KEY = "ВСТАВ_СЮДИ_СВІЙ_TMDB_KEY"
 
+
+# мова отримання даних
+LANG = "uk-UA"
+
+
+# сайт uCoz
+SITE = "5star.at.ua"
+
+
+# категорії uCoz
+
+CATEGORIES = {
+
+    "Action": 13,
+    "Mystery": 3,
+    "Comedy": 9,
+    "Fantasy": 10,
+    "Horror": 11,
+    "Drama": 12
+
+}
